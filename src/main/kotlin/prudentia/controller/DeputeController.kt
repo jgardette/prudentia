e968349732da9b41cf63c22453388e2a6e4fb2f7
Julien Gardette <gardette.julien@gmail.com>
2017-11-26 22:32:51 +0100

    debut de mapping Json

--- src/main/kotlin/prudentia/controller/DeputeController.kt
+++ src/main/kotlin/prudentia/controller/DeputeController.kt
@@ -1,10 +1,13 @@
 package prudentia.controller
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonInclude
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import prudentia.model.Depute
+import prudentia.model.Adresses
+import prudentia.model.Export
 import java.io.File
 
 
@@ -13,11 +16,11 @@ import java.io.File
 class DeputeController {
     /** Get list of deputes */
     @RequestMapping("/deputes")
-    fun getDeputes() : String {
+    fun getDeputes() : List<Adresses> {
         val JSON = jacksonObjectMapper()
         val file = File("src/main/files/AMO10_deputes_actifs_mandats_actifs_organes_XIV.json")
-        val myList = JSON.readValue<List<Depute>>(file)
-        return myList.toString()
+        val result = JSON.readValue<Export>(file)
+        return result.export.acteurs.acteur
     }
 
 
--- src/main/kotlin/prudentia/model/Depute.kt
+++ src/main/kotlin/prudentia/model/Depute.kt
@@ -1,6 +1,69 @@
 package prudentia.model
 
-data class Depute(
-        val nom: String,
-        val prenom: String
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Export(
+        @JsonProperty("export")
+        val export: Acteurs
 )
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Acteurs(
+        @JsonProperty("acteurs")
+        val acteurs: Acteur
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Acteur(
+        @JsonProperty("acteur")
+        val acteur: List<Adresses>
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Adresses(
+        @JsonProperty("adresses")
+        val adresses: Adresse,
+
+        @JsonProperty("etatCivil")
+        val etatCivil: EtatCivil
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class EtatCivil(
+        @JsonProperty("civ")
+        val civ: String?,
+
+        @JsonProperty("prenom")
+        val prenom: String?,
+
+        @JsonProperty("nom")
+        val nom: String?
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Adresse(
+        @JsonProperty("adresse")
+        val adresse: List<InfosAdresse>
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class InfosAdresse(
+        @JsonProperty("codePostal")
+        val codePostal: String?,
+
+        @JsonProperty("intitule")
+        val intitule: String?,
+
+        @JsonProperty("numeroRue")
+        val numeroRue: String?,
+
+        @JsonProperty("nomRue")
+        val nomRue: String?,
+
+        @JsonProperty("ville")
+        val ville: String?
+        )
