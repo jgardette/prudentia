aeecadcc2472c11b5ce58feb3be04e5bfecc72cc
Julien Gardette <j5492520@saint-gobain.com>
2018-01-19 13:39:34 +0100

    fix: unused imports

--- src/main/kotlin/prudentia/Application.kt
+++ src/main/kotlin/prudentia/Application.kt
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import prudentia.controller.DeputeController
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import prudentia.controller.DeputeController
 
 
 /**
